94832b9eaef98e761408293255fa54c302db04f9
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-06-24 17:37:49 +0200

    upd aggiunto costruttore

--- index.php
+++ index.php
@@ -12,13 +12,26 @@
         </h1>
         <?php
         // classe movie
-        class Movie {
-            // variabili dell'istanza
-            public $style;
-            public $director;
-            public $year;
-            public $genere;
+       class Movie {
+    // Variabili d'istanza
+    public $title;
+    public $director;
+    public $year;
+    public $genre;
 
+    // Costruttore
+    public function __construct($title, $director, $year, $genre) {
+        if (empty($title) || empty($director) || empty($year) || empty($genre)) {
+            throw new Exception("All fields are required.");
+        }
+        if (!is_numeric($year) || $year < 1888 || $year > intval(date("Y"))) {
+            throw new Exception("Year must be a valid number between 1888 and the current year.");
+        }
+        $this->title = $title;
+        $this->director = $director;
+        $this->year = $year;
+        $this->genre = $genre;
+    }
 }
         ?>
     </main>
