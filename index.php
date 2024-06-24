edac8b76314554a360e33f800ae17fc23c4bb76f
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-06-24 16:49:13 +0200

    upd creato scheletro del programma

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>opp movies</title>
+</head>
+<body>
+    <main>
+        <h1>
+            PHP OPP Movies
+        </h1>
+        <?php
+
+        ?>
+    </main>
+</body>
+</html>
